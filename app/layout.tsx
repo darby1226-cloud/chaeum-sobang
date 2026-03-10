61863bb8d0ff9577b7a1a92ef2b3fe35a74a1915
bluebeen <68265438+bluebeen@users.noreply.github.com>
2026-03-10 16:01:14 +0900

    vercel reload

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,11 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://www.chaeum119.co.kr",
   },
+  verification: {
+    other: {
+      "naver-site-verification": "0b5d8c1581790c7a7a37acd294987f899259b1b5",
+    },
+  },
 }
 
 export default function RootLayout({
